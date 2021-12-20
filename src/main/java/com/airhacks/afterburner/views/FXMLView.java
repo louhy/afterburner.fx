15777897c5d50a9131aa0cdb7df1a841f393ceb1
Lou H <ehamers@protonmail.com>
2021-12-19 23:25:04 -0500

    Remove System.err.log() output for FXML file lookups

--- src/main/java/com/airhacks/afterburner/views/FXMLView.java
+++ src/main/java/com/airhacks/afterburner/views/FXMLView.java
@@ -211,7 +211,7 @@ public abstract class FXMLView extends StackPane {
         if (found != null) {
             return name;
         }
-        System.err.println("File: " + name + " not found, attempting with camel case");
+        
         name = getConventionalName(false, ending);
         found = getClass().getResource(name);
         if (mandatory && found == null) {
